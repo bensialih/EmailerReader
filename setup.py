ed44c82259482611a2655afe809e7818b6306cdd
Abdel Bensiali <bensialih@gmail.com>
2017-04-23 15:42:12 +0100

    added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(name='EmailParser',
+      version='1.0',
+      description='Reads mailbox & parses emails',
+      author='Abdel Hakim Bensiali',
+      author_email='bensialih@gmail.com',
+      url='http://www.http://bensialih.co.uk/bensialih/content/emailparser',
+      py_modules=['appdirs','beautifulsoup4','packaging', 'pyparsing', 'QueryableList', 'six'],
+	)
