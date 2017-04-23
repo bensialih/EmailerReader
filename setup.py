157823ee42d7149f8a7dc471f149dd50280e9d85
Abdel Bensiali <bensialih@gmail.com>
2017-04-23 16:56:24 +0100

    setup.py using setuptools

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
 
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Email',
+    'author': 'Abdel Hakim Bensiali',
+    'url': 'http://bensialih.co.uk/bensialih/content/emailparser',
+    'download_url': 'https://github.com/bensialih/EmailerReader',
+    'author_email': 'bensialih@gmail.com',
+    'version': '0.1',
+    'install_requires': ['appdirs','beautifulsoup4','packaging', 'pyparsing', 'QueryableList', 'six'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'email_parser'
+}
+
+setup(**config)
+
+
 from distutils.core import setup
 
-setup(name='EmailParser',
-      version='1.0',
-      description='Reads mailbox & parses emails',
-      author='Abdel Hakim Bensiali',
-      author_email='bensialih@gmail.com',
-      url='http://www.http://bensialih.co.uk/bensialih/content/emailparser',
-      py_modules=['appdirs','beautifulsoup4','packaging', 'pyparsing', 'QueryableList', 'six'],
-	)
